45bd9dfe48554de2f791bfe8ba9aabc35a19d3ca
LennyFitzgerald <lennyfitzgerald0422@gmail.com>
2019-02-17 20:51:54 +0900

    Initial commit

---
+++ app/src/main/java/jp/techacademy/wakahara/koumei/bistaskapp/TaskApp.kt
@@ -0,0 +1,11 @@
+package jp.techacademy.wakahara.koumei.bistaskapp
+
+import android.app.Application
+import io.realm.Realm
+
+class TaskApp: Application() {
+    override fun onCreate() {
+        super.onCreate()
+        Realm.init(this)
+    }
+}
